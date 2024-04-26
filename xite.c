611b917f445be39ac60cacba51c44da8e147f972
Prakshaal Jain <prakshaal@gmail.com>
2024-04-27 01:56:59 +0530

    Raw input and printing ascii char byte by byte

--- xite.c
+++ xite.c
@@ -1,3 +1,36 @@
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+struct termios orig_termios;
+
+void disableRawMode(){
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+  struct termios raw = orig_termios;
+  raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main(){
+
+    enableRawMode();
+
+    char c;
+    while( read (STDIN_FILENO, &c , 1) == 1 && c != 'q'){
+        if (iscntrl(c)){
+            printf("%d\n",c);
+        }
+        else{
+            printf("%d('%c')\n",c ,c ); 
+        }
+    }
     return 0;
 }
